95fe0fe2f3c53dd1ace92cc4e95ff1fec448780b
Elan Ruusamäe <glen@delfi.ee>
2013-03-03 14:34:49 +0000

    html5 notifications enabled radiomania on air

---
+++ mania.php
@@ -0,0 +1,34 @@
+<?php
+
+$url = 'http://www.mania.ee/eeter.php';
+$content = file_get_contents($url);
+if ($content === false) {
+	die('Error fetching');
+}
+
+// <font color='ffffff'><b>THE SUN</b>,<br>Hingede müüja  (2012)<br>(16:13)</font></body></html>
+// json_encode() requires UTF8, and so our webpage
+$content = iconv('latin1', 'utf8', $content);
+
+if (preg_match('#<font[^>]+><b>(?P<artist>.+)</b>,<br>(?P<title>.+)<br>\((?P<time>[\d:]+)\)</font>#U', $content, $m)) {
+	$res = array();
+	foreach ($m as $key => $value) {
+		if (!is_numeric($key)) {
+			$res[$key] = $value;
+		}
+	}
+	$ret = json_encode($res);
+
+	$callback = isset($_REQUEST['callback']) ? trim($_REQUEST['callback']) : null;
+
+	if ($callback) {
+		header("Content-type: text/javascript");
+		echo $callback, '(', $ret, ');';
+	} else {
+		header('Content-type: application/json');
+		echo $ret;
+	}
+
+} else {
+	echo $contents;
+}
